ab285ad6039ac3516ff184f0762317de1e0c2036
Vinzenz Hersche <hersche@openmailbox.org>
2018-08-22 05:27:19 +0200

    small fix

--- resources/assets/js/welcome.js
+++ resources/assets/js/welcome.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
     var first=true;
     $.each( data.data, function( key, value ) {
       val1 = value;
-      items += '<div style="min-width: 300px;" class="col-lg-4 col-md-4 col-xs-6 card"><a href="{{ url("/media/") }}/'+val1.title+'" class="d-block h-100"><img class="card-img-top" src="'+ val1.poster_source + '" alt=""><div class="card-img-overlay"><h4 class="card-title bg-secondary text-info" style="opacity: 0.9;">'+val1.title+'</h4></div></a></div></div></div>';
+      items += '<div style="min-width: 300px;" class="col-lg-4 col-md-4 col-xs-6 card"><a href="'+baseUrl+'media/'+val1.title+'" class="d-block h-100"><img class="card-img-top" src="'+ val1.poster_source + '" alt=""><div class="card-img-overlay"><h4 class="card-title bg-secondary text-info" style="opacity: 0.9;">'+val1.title+'</h4></div></a></div></div></div>';
     });
     $("#carouselBody").html(items);
 });
