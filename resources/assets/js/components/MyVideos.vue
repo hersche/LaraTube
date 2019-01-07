541e98a549fd4d91a8a1bccd46193cedfcf16f4f
Vinzenz Hersche <hersche@openmailbox.org>
2019-01-07 05:29:18 +0100

    add delete to myvideos

--- resources/assets/js/components/MyVideos.vue
+++ resources/assets/js/components/MyVideos.vue
@@ -2,9 +2,10 @@
   <div>
     <h3>My Videos</h3>
     <div class="row text-center text-lg-left" id="profilevideos">
-    <div v-for="item in medias" v-if="item.user_id==loggeduserid"  class="col-lg-3 col-md-3 col-xs-6">
+    <div v-for="item in medias" v-if="item.user_id==loggeduserid"  class="col-lg-4 col-md-4 col-xs-6">
         <singleField v-bind:item="item" v-bind:loggeduserid="loggeduserid"></singleField>
-        <router-link class="btn btn-sm btn-info float-right" :to="'/mediaedit/'+encodeURIComponent(item.title)">Edit</router-link>
+        <router-link class="btn btn-sm btn-info float-left" :to="'/mediaedit/'+encodeURIComponent(item.title)">Edit</router-link>
+        <button class="btn btn-sm btn-danger float-right" @click="openConfirm(item.id,item.title)" >Delete</button>
     </div>
 
     </div>
@@ -15,10 +16,47 @@
   import SingleGalleryField from './SingleGalleryField'
   export default {
     props: ['medias','baseUrl','canloadmore','loggeduserid'],
+    data(){
+      return {
+        tmpid: 0,
+        tmptitle:''
+
+      }
+    },
     methods: {
+      openConfirm(id,title){
+        this.tmpid = id;
+        this.tmptitle = title
+        this.$vs.dialog({
+          type:'confirm',
+          color: 'danger',
+          title: `Delete media?`,
+          text: 'Delete a media can not be reverted. Are you shure?',
+          accept:this.deleteAction
+        })
+      },
       emitLoadMore() {
         eventBus.$emit('loadMore','');
-      }
+      },
+      deleteAction() {
+        let that = this;
+        $.ajax({
+            url: '/internal-api/media/'+that.tmpid,
+            type: 'DELETE',
+            cache: false,
+            contentType: false,
+            processData: false,
+            complete : function(res) {
+              if(res.status==200){
+                //eventBus.$emit('showAlert',['success','Video uploaded']);
+
+              }
+              eventBus.$emit('videoDeleted',that.tmptitle);
+            }
+
+        });
+        return false;
+      },
     },
     mounted(){
       eventBus.$emit('loadUserVideos',this.loggeduserid);
