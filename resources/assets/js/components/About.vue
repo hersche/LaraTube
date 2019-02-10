d36ccbb3eb07fc57b489fd20bd96e149b91ceee0
Vinzenz Hersche <vinzenz.hersche@gmail.com>
2019-02-10 02:23:55 +0100

    extend about

--- resources/assets/js/components/About.vue
+++ resources/assets/js/components/About.vue
@@ -1,24 +1,29 @@
 <template>
   <div>
-<h2>About LaraTube</h2>
-<p>LaraTube was created because of security-ascpects, for develop a own, indipendend project and finaly: To learn Laravel and Vue.</p>
-<p>It is a work in progress, a pre-alpha!</p>
-<h3>Credits</h3>
-<p>
-  This project uses a lot of ressources to be created. Thanks to all of them!
-  <ul>
-  <li>Laravel</li>
-  <li>Vue</li>
-  <li>VueSax</li>
-  <li>Bootstrap vue</li>
-  <li>Stackoverflow</li>
-  <li>ApexCharts</li>
-  <li>Webtorrent</li>
-  <li>hootlex/laravel-friendships</li>
-  <li>james-heinrich/getid3</li>
-  <li>rtconner/laravel-tagging</li>
-  <li>spatie/laravel-permission</li>
-</ul>
-</p>
-</div>
+    <h2>About LaraTube</h2>
+    <p>LaraTube was created because of security-ascpects, for develop a own, indipendend project and finaly: To learn Laravel and Vue.</p>
+    <p>It is a work in progress, a pre-alpha!</p>
+    <h3>Gitlab</h3>
+    <p>Check out the project's source on 
+      <a href="https://gitlab.com/hersche/LaraTube" target="_blank">gitlab.com/hersche/LaraTube</a>
+    </p>
+    <p>Read the Readme.md-file there for more information</p>
+    <h2>Credits</h2>
+    <p>
+      This project uses a lot of ressources to be created. Thanks to all of them!
+      <ul>
+        <li><a href="https://laravel.com/" target="_blank">Laravel</a></li>
+        <li><a href="https://vuejs.org/" target="_blank">Vue</a>/li>
+        <li><a href="https://lusaxweb.github.io/vuesax/" target="_blank">VueSax</a></li>
+        <li><a href="https://bootstrap-vue.js.org/" target="_blank">Bootstrap vue</a></li>
+        <li><a href="https://stackoverflow.com/" target="_blank">Stackoverflow</a></li>
+        <li>ApexCharts</li>
+        <li><a href="https://webtorrent.io/" target="_blank">Webtorrent</a></li>
+        <li>hootlex/laravel-friendships</li>
+        <li>james-heinrich/getid3</li>
+        <li>rtconner/laravel-tagging</li>
+        <li>spatie/laravel-permission</li>
+      </ul>
+    </p>
+  </div>
 </template>
